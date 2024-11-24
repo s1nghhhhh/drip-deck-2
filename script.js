57c7f97af5daee261591bea370e4a8c8318f351d
s1nghhhhh <singhshreyansh2354@gmail.com>
2024-11-24 15:59:01 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+//DOM
+const swiper = document.querySelector('#swiper');
+
+//constants
+const urls = [
+    'images/outfit1.jpg',
+    'images/outfit2.jpg',
+    'images/outfit3.jpg',
+    'images/outfit4.jpg',
+    'images/outfit5.jpg',
+    'images/outfit6.jpg'
+]
+
+
+// variables
+let cardCount = 0;
+
+// functions
+function appendNewCard() {
+  const card = new Card({
+    imageUrl: urls[cardCount % 5],
+    onDismiss: appendNewCard,
+    onLike: () => {
+      like.style.animationPlayState = 'running';
+      like.classList.toggle('trigger');
+    },
+    onDislike: () => {
+      dislike.style.animationPlayState = 'running';
+      dislike.classList.toggle('trigger');
+    }
+  });
+  swiper.append(card.element);
+  cardCount++;
+
+  const cards = swiper.querySelectorAll('.card:not(.dismissing)');
+  cards.forEach((card, index) => {
+    card.style.setProperty('--i', index);
+  });
+}
+
+// first 5 cards
+for (let i = 0; i < 5; i++) {
+  appendNewCard();
+}
