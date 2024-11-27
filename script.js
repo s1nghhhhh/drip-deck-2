f34fa640888903606fef13cd505e7e90ac6e0825
s1nghhhhh <singhshreyansh2354@gmail.com>
2024-11-28 01:54:17 +0530

    added like counter

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //DOM
 const swiper = document.querySelector('#swiper');
+const likesContainer = document.querySelector('#likes-container');
 
 //constants
 const urls = [
@@ -9,24 +10,58 @@ const urls = [
     'images/outfit4.jpg',
     'images/outfit5.jpg',
     'images/outfit6.jpg'
-]
+];
 
+// Track likes for each image
+const likesCount = {
+    'images/outfit1.jpg': 0,
+    'images/outfit2.jpg': 0,
+    'images/outfit3.jpg': 0,
+    'images/outfit4.jpg': 0,
+    'images/outfit5.jpg': 0,
+    'images/outfit6.jpg': 0
+};
 
 // variables
 let cardCount = 0;
 
+// Function to update likes display
+function updateLikesDisplay() {
+    // Clear previous likes display
+    likesContainer.innerHTML = '';
+
+    // Create likes display for each image
+    urls.forEach(url => {
+        const likesItem = document.createElement('div');
+        likesItem.classList.add('likes-item');
+
+        const img = document.createElement('img');
+        img.src = url;
+
+        const likesCountElement = document.createElement('div');
+        likesCountElement.classList.add('likes-count');
+        likesCountElement.textContent = `Likes: ${likesCount[url]}`;
+
+        likesItem.append(img, likesCountElement);
+        likesContainer.appendChild(likesItem);
+    });
+}
+
 // functions
 function appendNewCard() {
   const card = new Card({
-    imageUrl: urls[cardCount % 5],
+    imageUrl: urls[cardCount % urls.length],
     onDismiss: appendNewCard,
     onLike: () => {
-      like.style.animationPlayState = 'running';
-      like.classList.toggle('trigger');
+      // Increment likes for the current image
+      likesCount[urls[cardCount % urls.length]]++;
+      
+      // Update likes display
+      updateLikesDisplay();
     },
     onDislike: () => {
-      dislike.style.animationPlayState = 'running';
-      dislike.classList.toggle('trigger');
+      console.log('Image disliked');
+      // Optional: Track dislikes if needed
     }
   });
   swiper.append(card.element);
@@ -37,6 +72,9 @@ function appendNewCard() {
     card.style.setProperty('--i', index);
   });
 }
+
+// Initial likes display
+updateLikesDisplay();
 
 // first 5 cards
 for (let i = 0; i < 5; i++) {
